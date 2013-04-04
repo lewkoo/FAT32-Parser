f4090d7dd913c2dea00e75e94b0ce3ed42609a69
lewkoo <lewkoo@gmail.com>
2013-04-03 21:59:21 -0500

    dir struct finished

--- OS_A3/OS_A3/dir.h
+++ OS_A3/OS_A3/dir.h
@@ -8,7 +8,31 @@
 
 #ifndef OS_A3_dir_h
 #define OS_A3_dir_h
+#include <inttypes.h>
+
+/* dir info constants */
+#define DIR_NAME_LENGTH 11
 
 
+
+#pragma pack(push)
+#pragma pack(1)
+struct fat32BS_struct {
+    char DIR_Name[DIR_NAME_LENGTH];
+    uint8_t DIR_Attr;
+    uint8_t DIR_NTRes;
+    uint16_t DIR_CrtTime;
+    uint16_t DIR_CrtDate;
+    uint16_t DIR_LstAccDate;
+    uint16_t DIR_FstClusHI;
+    uint16_t DIR_WrtTime;
+    uint16_t DIR_WrtDate;
+    uint16_t DIR_FstClusLO;
+    uint32_t DIR_FileSize;
+    
+};
+#pragma pack(pop)
+
+typedef struct fat32BS_struct fat32BS;
 
 #endif
